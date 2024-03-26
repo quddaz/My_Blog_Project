b87714d6f789fe32951b86177c668536650753cd
김병웅 <quddnddl35@naver.com>
2024-03-26 14:48:03 +0900

    엔티티 구현 0326

---
+++ src/main/java/quddaz/myblog/domain/DTO/JoinDTO.java
@@ -0,0 +1,24 @@
+package quddaz.myblog.domain.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+public class JoinDTO {
+  @NotBlank(message = "이름은 필수 입력 값입니다.")
+  @Length(min = 3,max = 16,message = "아이디를 8자 이상, 16자 이하로 입력해주세요")
+  private String id;
+
+  @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+  @Length(min = 8,max = 16,message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
+  private String password;
+
+  @NotEmpty(message = "전화번호는 필수 입력 값입니다.")
+  @Pattern(regexp = "^\\d{3}-\\d{4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다.")
+  private String tell;
+}
---
+++ src/main/java/quddaz/myblog/domain/Member.java
@@ -0,0 +1,21 @@
+package quddaz.myblog.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+  @Id
+  public String id;
+  public String password;
+  public String tell;
+
+  public Member(){};
+}
